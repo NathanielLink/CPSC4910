c5ee75d4e1c22e959968b89117711821d71b762f
AndrewHGresham <agresha@clemson.edu>
2017-03-17 12:58:03 -0400

    Add files via upload

--- Survey/4910/src/Survey/Feedback.java
+++ Survey/4910/src/Survey/Feedback.java
@@ -6,7 +6,6 @@ import java.sql.*;
 import javax.swing.JOptionPane;
 
 
-
 // Yes/No pop-up survey for user to complete when they finish their route
 public class Feedback {
 		
@@ -19,6 +18,7 @@ public class Feedback {
 		
 		userResponse = JOptionPane.showConfirmDialog(null, "Did you enjoy your route?");
 		
+		// wrap around insertion statements below when ready
 		if (userResponse == 0 || userResponse == 1) {
 			JOptionPane.showMessageDialog(null, "Thank you for your feedback!");
 		}
@@ -48,14 +48,41 @@ public class Feedback {
 		//We don't currently have a way to pull this information but it would be useful to have eventually.
 		float startCharge = 0, endCharge = 0, addedTime = 0;
 		
-		String query = "insert into Responses(vehicle_make, vehicle_model, vehicle_year, start_Lat," +
+		//This inserts all of the column data for the responses table if the user answers the survey
+		String Response = "insert into Responses(vehicle_make, vehicle_model, vehicle_year, start_Lat," +
 		               " Start_Long, End_Lat, End_Long, Start_Charge, End_Charge, Added_Time, Response)" + 
+				
 				       " values(Chevrolet, Volt, 2017, " + startLat + ", " + endLat + ", " + startLong + ", " +
 		               endLong + ", " + startCharge + ", " + endCharge + ", " + addedTime + ", " + userResponse + ")";
-		               
+		PreparedStatement ResponseStatement = connect.prepareStatement(Response);
+		ResponseStatement.execute();
 		
-		PreparedStatement preparedStatement = connect.prepareStatement(query);
-		preparedStatement.execute();
+
+		//This pulls the most recent unique identifier within the Responses SQL table
+		//this should always be the identifier corresponding to the insert statement above
+		//this will only not work if by the time the code executes this select statement 
+		//another row has been inserted into the Response table
+		int Trip_id = 0;
+		Statement LookupTrip_id = connect.createStatement();
+		LookupTrip_id.executeQuery("SELECT TOP 1 id FROM Responses order by id desc");	
+		ResultSet rs = LookupTrip_id.getResultSet();
+		while(rs.next()){
+		  	Trip_id = rs.getInt("id");
+		}
+		rs.close();
+		LookupTrip_id.close();
+		
+		
+		//this needs to be within a loop. there should be a new record in the Trips table for every inductive 
+		//charging road the user took. The corresponding Trip_id will always be the same.
+		float Charging_Start_Lat = 0, Charging_Start_Long = 0, Charging_End_Lat = 0, Charging_End_Long = 0;
+		
+		String Trips = "insert into Trips(Trip_id, Start_Lat, Start_Long, End_Lat, End_Long)" +
+		
+						" values(" + Trip_id + ", " + Charging_Start_Lat + ", " + Charging_Start_Long + 
+						", " + Charging_End_Lat + ", " + Charging_End_Long + ")";
+		PreparedStatement TripsStatement = connect.prepareStatement(Trips);
+		TripsStatement.execute();
 		
 		connect.close();
 		
